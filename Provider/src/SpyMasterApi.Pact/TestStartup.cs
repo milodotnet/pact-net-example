53af07d11bc7e57fcc42517a5df50fd9f102a6e4
Sasa Milovic <sasa.milovic@asos.com>
2019-06-28 11:53:11 +0100

    adding a builder and moving provider state setup into middleware for now

--- Provider/src/SpyMasterApi.Pact/TestStartup.cs
+++ Provider/src/SpyMasterApi.Pact/TestStartup.cs
@@ -21,21 +21,12 @@ namespace SpyMasterApi.Pact
         {
             _apiStartup.ConfigureServices(services);
             services.AddSingleton<IAgentsService, InMemoryAgentsService>();
+            services.AddSingleton<SpyMasterInMemoryProviderStateSeeder, SpyMasterInMemoryProviderStateSeeder>();
         }
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            var providerStateSeeder = ConfigureProviderStates();
-            app.UseMiddleware<SpyMasterProviderStateMiddleware>(providerStateSeeder);
+            app.UseMiddleware<SpyMasterProviderStateMiddleware>();
             _apiStartup.Configure(app, env);
-        }
-
-        private static SpyMasterInMemoryProviderStateSeeder ConfigureProviderStates()
-        {
-            var providerStateSeeder = new SpyMasterInMemoryProviderStateSeeder();
-            providerStateSeeder
-                .AddProviderStateSetup(new ProviderState("SpyLens FrontEnd", "An agent '007' exists"),
-                    service => service.Add(new AgentDetails("Roger", "Moore", new DateTime(1968, 03, 02), 80)));
-            return providerStateSeeder;
         }
     }
 }
